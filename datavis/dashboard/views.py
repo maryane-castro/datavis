03e4f976b8c92e000ce744ff701a23071833c611
Maryane de Castro <122397850+maryane-castro@users.noreply.github.com>
2023-05-23 21:08:46 -0300

    Update views.py

--- datavis/dashboard/views.py
+++ datavis/dashboard/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Cadastro
 # Create your views here.
